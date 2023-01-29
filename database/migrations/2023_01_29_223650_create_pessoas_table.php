083ab19a818263f348734c19a4bb3266d7f4a597
Edgarvital <edgar.tecnico@hotmail.com>
2023-01-29 19:51:13 -0300

    Criação de Model e Migrate de Pessoa

---
+++ database/migrations/2023_01_29_223650_create_pessoas_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Pessoa;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     * 'nome',
+    'telefone',
+    'documento',
+    'user_id',
+    'status'
+     */
+    public function up()
+    {
+        Schema::create('pessoas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('nome');
+            $table->string('telefone')->unique();
+            $table->string('documento')->unique();
+            $table->enum('status', [Pessoa::status[0], Pessoa::status[1], Pessoa::status[2]]);
+            $table->foreignId('user_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pessoas');
+    }
+};
